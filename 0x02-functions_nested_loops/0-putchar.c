867ed53f8b2cdcb6f5a0061204830d66dd77f7c4
Tewodros Tesfaye <tmt2164@gmail.com>
2022-09-13 01:28:12 -0700

    Exe 2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char T[10] = "Tewodros\n";
+int i = 0;
+
+for (i = 0; i < 10; i++)
+{
+_putchar(T[i]);
+}
+return (0);
+}
