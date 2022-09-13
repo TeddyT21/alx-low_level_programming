cc48bfb3e846c49088b2fa8377c9db4bea4bcd58
TeddyT21 <111092662+TeddyT21@users.noreply.github.com>
2022-09-13 23:45:09 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 #define LARGEST 10000000000
 /**
  * main - main block
- * Description: Print the first 98 fibonacci numbers starting with 1.
+ * Description: Find and print the first 98 fib numbers starting with 1 and 2.
  * Numbers should be separated by coma and space.
  * Return: 0
  */
@@ -32,15 +32,6 @@ printf("%lu", bk2);
 }
 if (count != 97)
 printf(", ");
-}
-printf("\n");
-return (0);
-}
-} */
-if (i != 95)
-{
-printf(", ");
-}
 }
 printf("\n");
 return (0);
