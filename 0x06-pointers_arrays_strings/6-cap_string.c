7402dd289054feb8ccfb42ba284b9fcf1a82f296
Tewodros Tesfaye <tmt2164@gmail.com>
2022-09-21 13:39:08 -0700

    q6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,6 +3,7 @@
 /**
  * cap_string - capitalizes all words of a string.
  * @s: string to use.
+ * 
  * Return: string.
  */
 char *cap_string(char *s)
@@ -14,24 +15,24 @@ char *cap_string(char *s)
 		s[0] -= 32;
 	while (s[i] != '\0')
 	{
-	if (s[i] > 96 && s[i] < 123)
-	{
-		j = 0;
-		check = 0;
-		while (check == 0 && j < 13)
+		if (s[i] > 96 && s[i] < 123)
 		{
-		if (s[i - 1] == a[j])
-		{
-			check = 1;
+			j = 0;
+			check = 0;
+			while (check == 0 && j < 13)
+			{
+				if (s[i - 1] == a[j])
+				{
+					check = 1;
+				}
+				j++;
+			}
+			if (check == 1)
+			{
+				s[i] -= 32;
+			}
 		}
-		j++;
-		}
-		if (check == 1)
-		{
-			s[i] -= 32;
-		}
-	}
-	i++;
+		i++;
 	}
 	return (s);
 }
