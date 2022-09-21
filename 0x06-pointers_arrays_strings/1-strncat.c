e779238fb090979321867a3ba3e35f0efc9ba80b
Tewodros Tesfaye <tmt2164@gmail.com>
2022-09-21 12:55:04 -0700

    q1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,7 +10,6 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, k = 0;
-
 	while (dest[i] != '\0')
 		i++;
 	while (src[k] != '\0' && n > k)
@@ -21,7 +20,7 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	if (n > 0)
 	{
-		dest[i] = '\0'; 
+		dest[i] = '\0';
 	}
 	return (dest);
 }
