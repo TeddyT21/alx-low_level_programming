12b1e858255f15a09fb56a99f848d421600f4507
TeddyT21 <111092662+TeddyT21@users.noreply.github.com>
2022-09-22 07:43:55 +0300

    q104
    q104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,42 +8,41 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, pi, c;
+	int o, j, i;
+
+	o = 0;
 
 	if (size <= 0)
-		printf("\n");
-	else
 	{
-		for (i = 9, pi = 0; i < size; i += 10)
-		{	printf("%.8x: ", pi);
-				for (c = pi; c <= i ; c++)
-				{	printf("%.2x", b[c]);
-					if (c % 2 == 1)
-						printf(" ");	}
-				for (c = pi; c <= i ; c++)
-				{
-					if (b[c] < 32)
-						printf(".");
-					else
-						printf("%c", b[c]);	}
-					printf("\n");
-					pi = i + 1;	}
-		if (pi < size - 1)
-		{	i -= 9;
-			printf("%.8x: ", i);
-			for (c = i; c < i + 10; c++)
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
 			{
-				if (c < size)
-					printf("%.2x", b[c]);
-				else
-					printf("  ");
-				if (c % 2 == 1)
-					printf(" ");	}
-			for (; i < size ; i++)
-			{
-				if (b[i] < 32)
-					printf(".");
-				else
-					printf("%c", b[i]);
+				printf(" ");
 			}
-			printf("\n");	}	}	}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
+}
