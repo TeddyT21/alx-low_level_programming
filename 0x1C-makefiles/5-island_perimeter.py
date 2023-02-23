66833a3a38d6abab6bc4c8a6b66d077ae9cef422
TeddyT21 <111092662+TeddyT21@users.noreply.github.com>
2023-02-23 12:03:02 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""This module contains one function island_perimeter(grid):"""
+"""5-island_perimeter module"""
 
 
 def island_perimeter(grid):
     """Returns the perimeter of the island described in grid"""
-    prm = 0
-    for row in grid:
-        for nb in row:
-            if nb == 1:
-                prm += 2
-    if prm > 0:
-        prm += 2
-    return prm
+    c = 0
+    length = len(grid) - 1
+    width = len(grid[0]) - 1
+
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    c += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    c += 1
+                if j == width or grid[i][j + 1] != 1:
+                    c += 1
+                if i == length or grid[i + 1][j] != 1:
+                    c += 1
+return c
